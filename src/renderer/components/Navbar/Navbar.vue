aa5cee24b99fbef20fe3f4075271d77cd5cd03c4
Adr1GR <adriangelmotto@gmail.com>
2025-10-23 14:18:32 +0200

    Added nabar and removed default one from electron

--- src/renderer/components/Navbar/Navbar.vue
+++ src/renderer/components/Navbar/Navbar.vue
@@ -1,9 +1,55 @@
 <template>
-  <h1 class="">navbar placeholder</h1>
+  <!-- Navbar móvil -->
+  <nav
+    class="fixed bottom-0 left-0 w-full h-14 sm:hidden z-9999 bg-red-600 text-white flex items-center justify-around"
+  >
+    <a
+      class="flex flex-col items-center"
+      href="https://github.com/Adr1GR/PokedexVue"
+      target="_blank"
+    >
+      <span class="text-sm">Source</span>
+    </a>
+    <RouterLink
+      :to="{ name: 'pokemon-list' }"
+      class="flex flex-col items-center"
+    >
+      <span class="text-sm">PokedexVue</span>
+    </RouterLink>
+    <RouterLink
+      :to="{ name: 'pokemon-list' }"
+      class="flex flex-col items-center"
+    >
+      <span class="text-sm">About</span>
+    </RouterLink>
+  </nav>
+
+  <!-- Navbar escritorio -->
+  <nav
+    id="navbar"
+    class="hidden sm:flex bg-red-600 w-full items-center justify-between px-4 py-2 text-white shadow-xs shadow-red-950"
+  >
+    <RouterLink
+      class="py-2 px-4 leading-normal no-underline hover:bg-red-500"
+      :to="{ name: 'pokemon-list' }"
+      >PokedexVue</RouterLink
+    >
+    <div class="flex space-x-4">
+      <a
+        class="py-2 px-4 leading-normal no-underline hover:bg-red-500"
+        href="https://github.com/Adr1GR/PokedexVue"
+        target="_blank"
+        >Source</a
+      >
+      <RouterLink
+        class="py-2 px-4 leading-normal no-underline hover:bg-red-500"
+        :to="{ name: 'pokemon-list' }"
+        >About</RouterLink
+      >
+    </div>
+  </nav>
 </template>
 
 <script setup>
-console.log("navbar script");
+import { RouterLink } from "vue-router";
 </script>
-
-<style scoped lang="scss" src="./Navbar.scss"></style>
