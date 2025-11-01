f12b26593675af5b63714b4523e076f4d94b397f
Adr1GR <adriangelmotto@gmail.com>
2025-11-01 11:16:26 +0100

    Change: Improved getDominantColor performance

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-container">
+  <div ref="main-container" class="main-container">
     <NavBar />
     <RouterView
       class="mx-auto md:max-w-screen-2xl mb-24 sm:mb-14 px-4 2xl:px-0 py-6"
@@ -10,9 +10,12 @@
 <script setup>
 import NavBar from "@/renderer/components/Navbar/Navbar.vue";
 import { useAppSettingsStore } from "@/renderer/stores/appSettingsStore";
+import { usePokemonStore } from "./stores/pokemonStore";
 
+const pokemonStore = usePokemonStore()
 const appSettings = useAppSettingsStore()
 
+pokemonStore.resetState()
 appSettings.initDefaults(true)
 </script>
 
